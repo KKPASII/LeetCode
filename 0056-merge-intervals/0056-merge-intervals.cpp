8b257cda1b215bcfe3f710ed66e3126883536455
KKPASII <90451738+KKPASII@users.noreply.github.com>
2024-09-24 23:57:54 +0900

    Time: 19 ms (85.62%), Space: 23 MB (37.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -12,7 +12,6 @@ public:
         for (int i = 1; i < intervals.size(); i++) {
             // intervals[i][0]이 right보다 크면 answer.push_back(intervals[i]) 하고, left&right 초기화
             // intervals[i][0]이 right보다 작거나 같으면 넘어가기 (아래로)
-            // intervals[i][1] 검사
             // intervals[i][1]이 right보다 크면 right = intervals[i][1], answer 마지막 원소 right 값 갱신
             if (right < intervals[i][0]) {
                 answer.push_back(intervals[i]);
