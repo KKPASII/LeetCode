01681e88f1180b180f3d108d598da5b4e202812e
KKPASII <90451738+KKPASII@users.noreply.github.com>
2024-09-24 23:39:33 +0900

    Time: 23 ms (61.21%), Space: 23.1 MB (37.6%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> answer;
+
+        sort(intervals.begin(), intervals.end());
+        int left = intervals[0][0], right = intervals[0][1];
+        answer.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            // intervals[i][0]이 right보다 크면 answer.push_back(intervals[i]) 하고, left&right 초기화
+            // intervals[i][0]이 right보다 작거나 같으면 넘어가기 (아래로)
+            // intervals[i][1] 검사
+            // intervals[i][1]이 right보다 크면 right = intervals[i][1]
+            // intervals[i][1]이 right보다 작거나 같으면 넘어가기
+            if (right < intervals[i][0]) {
+                answer.push_back(intervals[i]);
+                left = intervals[i][0];
+                right = intervals[i][1];
+                continue;
+            }
+
+            right = max(right, intervals[i][1]);
+            answer.back()[1] = right;
+        }
+
+        return answer;
+    }
+};
