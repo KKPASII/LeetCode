4850f4e36f06020604e96a56f5a3163db611bad0
Hamplz <90451738+KKPASII@users.noreply.github.com>
2024-09-27 00:31:12 +0900

    Time: 4 ms (42.01%), Space: 13.9 MB (81.81%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        int mid;
+
+        // [4, 0, 1, 2, 3]
+        // 왼쪽이 정렬된 경우, 오른쪽이 정렬된 경우
+        while (left <= right) {
+            mid = (left + right) / 2;
+            const int mid_num = nums[mid];
+            const int left_num = nums[left];
+            const int right_num = nums[right];
+
+            if (target == mid_num) return mid;
+            
+            if (left_num <= mid_num) {
+                if (left_num <= target && target <= mid_num)
+                    right = mid - 1;
+                else left = mid + 1;
+                    
+            }
+            else if (mid_num <= right_num) {
+                if (mid_num <= target && target <= right_num)
+                    left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
