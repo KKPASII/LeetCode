8cfef8173543786fbc5a187adc41020ad91bd6e9
Hamplz <90451738+KKPASII@users.noreply.github.com>
2025-02-26 13:13:37 +0900

    Time: 44 ms (51.14%), Space: 18 MB (10.14%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -3,9 +3,10 @@ class Solution:
         dp = [0] * (target + 1)
         dp[0] = 1
 
-        for i in range(1, target + 1):
+
+        for i in range(1, target+1):
             for num in nums:
                 if i >= num:
-                    dp[i] += dp[i - num]
+                    dp[i] += dp[i-num] # i를 만드는 방법의 개수 더하기
 
         return dp[target] 
