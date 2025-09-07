1ecae77b5da2e95fb3ddb00c29a83f3756233625
Hamplz <90451738+KKPASII@users.noreply.github.com>
2025-09-07 23:29:30 +0900

    Time: 6 ms (42.48%), Space: 43 MB (82.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -8,15 +8,15 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             alphabet[s.charAt(i)-'a']++;
+            alphabet[t.charAt(i)-'a']--;
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            alphabet[t.charAt(i)-'a']--;
-            if (alphabet[t.charAt(i)-'a'] < 0) {
+        for (int a : alphabet) {
+            if (a != 0) {
                 return false;
             }
         }
-
+        
         return true;
     }
 }
