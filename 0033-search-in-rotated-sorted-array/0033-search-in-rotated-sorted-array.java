68b94c87e0227cd22e7c52c4af06513f2fa125b8
Hamplz <90451738+KKPASII@users.noreply.github.com>
2026-05-07 22:02:20 +0900

    Time: 0 ms (100%), Space: 43.9 MB (26.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,16 +1,15 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
         int left = 0;
-        int right = nums.length-1;
-        
+        int right = nums.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
                 return mid;
             }
-            
+
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
@@ -28,7 +27,7 @@ class Solution {
                 }
             }
         }
-
+        
         return -1;
     }
 }
