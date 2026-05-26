6be082935b13e904fec327d56a543e76ea0487bd
Hamplz <90451738+KKPASII@users.noreply.github.com>
2026-05-26 18:17:34 +0900

    Time: 3 ms (60.46%), Space: 43.6 MB (41.71%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -5,7 +5,6 @@ class Solution {
         int left = 0;
         int right = 0;
         int startIdx = 0;
-        int cnt = 0;
 
         for (int i = 0; i < len; i++) {
             if (s.charAt(i) == '(') {
@@ -15,14 +14,11 @@ class Solution {
                 right++;
             }
 
-            cnt++;
-
             if (left == right) {
                 sb.append(s.substring(startIdx + 1, i));
                 left = 0;
                 right = 0;
-                startIdx = i+1;
-                cnt = 0;
+                startIdx = i + 1;
             }
         }
 
