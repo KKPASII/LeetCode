88b173390a665e284003bae547e876d612673313
Hamplz <90451738+KKPASII@users.noreply.github.com>
2026-06-17 18:14:13 +0900

    Time: 37 ms (78.57%), Space: 58 MB (31.63%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -1,24 +1,50 @@
 class Solution {
     public int numTriplets(int[] nums1, int[] nums2) {
-        return tripletCount(nums1, nums2) + tripletCount(nums2, nums1); 
+        return tripletCount(nums1, nums2) + tripletCount(nums2, nums1);
     }
 
     public int tripletCount(int[] one, int[] two) {
-        HashMap<Long, Integer> map = new HashMap<>();
-        int cnt = 0;
+        HashMap<Integer, Integer> freqOne = new HashMap<>();
+        HashMap<Integer, Integer> freqTwo = new HashMap<>();
 
-        for (int i = 0; i < two.length; i++) {
-            for (int j = i + 1; j < two.length; j++) {
-                long production = (long) two[i] * two[j];
-                map.put(production, map.getOrDefault(production, 0) + 1);
+        for (int num : one) {
+            freqOne.put(num, freqOne.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : two) {
+            freqTwo.put(num, freqTwo.getOrDefault(num, 0) + 1);
+        }
+
+        HashMap<Long, Long> productCount = new HashMap<>();
+
+        List<Integer> values = new ArrayList<>(freqTwo.keySet());
+
+        for (int i = 0; i < values.size(); i++) {
+            for (int j = i; j < values.size(); j++) {
+                int a = values.get(i);
+                int b = values.get(j);
+
+                long product = (long) a * b;
+                long count;
+
+                if (a == b) {
+                    long f = freqTwo.get(a);
+                    count = f * (f - 1) / 2;
+                } else {
+                    count = (long) freqTwo.get(a) * freqTwo.get(b);
+                }
+
+                productCount.put(product, productCount.getOrDefault(product, 0L) + count);
             }
         }
 
-        for (int num : one) {
+        long result = 0;
+
+        for (int num : freqOne.keySet()) {
             long square = (long) num * num;
-            cnt += map.getOrDefault(square, 0);
+            result += (long) freqOne.get(num) * productCount.getOrDefault(square, 0L);
         }
-        
-        return cnt;
+
+        return (int) result;
     }
 }
