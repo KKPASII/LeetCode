8260527724f9fad96c73c0d4db9bfd8ea782c458
Hamplz <90451738+KKPASII@users.noreply.github.com>
2026-07-22 18:13:22 +0900

    Time: 0 ms (100%), Space: 47.1 MB (64.15%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.java
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -7,16 +7,17 @@ class Solution {
         int dc = 0;
 
         for (int i = 1; i < nums.length; i++) {
+            // 내림차순 발견
             if (nums[i - 1] > nums[i]) {
                 dc++;
-
+                // 고칠 곳이 2개 이상이면 false
                 if (dc > 1) {
                     return false;
                 }
-
+                // 현재 검사 중인 요소 기준 오름차순 조건에 맞으면
                 if (i == 1 || nums[i - 2] <= nums[i]) {
                     nums[i - 1] = nums[i];
-                }
+                } // 아니면
                 else {
                     nums[i] = nums[i - 1];
                 }
