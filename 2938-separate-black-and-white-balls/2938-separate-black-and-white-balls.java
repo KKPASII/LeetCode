f4d619c015c987c04ee310a79b24e40f2552e74f
Hamplz <90451738+KKPASII@users.noreply.github.com>
2026-07-24 17:23:56 +0900

    Time: 13 ms (5.07%), Space: 47.7 MB (32.55%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long cnt = 0;
+        int one = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                one++;
+            }
+        }
+
+        int startOneIndex = s.length() - one;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                cnt += startOneIndex - i;
+                startOneIndex++;
+            }
+        }
+
+        return cnt;
+    }
+}
