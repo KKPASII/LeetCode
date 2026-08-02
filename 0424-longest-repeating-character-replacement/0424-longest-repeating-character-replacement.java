a9a176fb0254311878f0241d236b129b0fdadb46
Hamplz <90451738+KKPASII@users.noreply.github.com>
2026-08-02 14:39:42 +0900

    Time: 8 ms (66.7%), Space: 46 MB (79.22%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -3,11 +3,14 @@ class Solution {
         int[] count = new int[26];
         int left = 0;
         int answer = 0;
+        int maxFreq = 0;
 
         for (int right = 0; right < s.length(); right++) {
             count[s.charAt(right) - 'A']++;
 
-            while ((right - left + 1) - getMaxFrequency(count) > k) {
+            maxFreq = Math.max(maxFreq, count[s.charAt(right) - 'A']);
+
+            while ((right - left + 1) - maxFreq > k) {
                 count[s.charAt(left) - 'A']--;
                 left++;
             }
@@ -17,15 +20,5 @@ class Solution {
         }
 
         return answer;
-    }
-
-    private int getMaxFrequency(int[] count) {
-        int maxFreq = 0;
-
-        for (int freq : count) {
-            maxFreq = Math.max(maxFreq, freq);
-        }
-
-        return maxFreq;
     }
 }
