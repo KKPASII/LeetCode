2207545c3839a68c26f7b66b08b54a77c9e62c04
Hamplz <90451738+KKPASII@users.noreply.github.com>
2026-08-25 17:02:13 +0900

    Time: 226 ms (5.58%), Space: 47 MB (61.93%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+
+        int answer = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] >= nums[i]) {
+                while (nums[i] <= nums[i-1]) {
+                    nums[i] += 1;
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
